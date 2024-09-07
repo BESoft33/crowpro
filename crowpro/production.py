cfef2a9b4b8be52eb34f6e676fce71e4a23375cc
BESoft33 <superstargajanikant@gmail.com>
2024-09-07 14:16:22 +0545

    added CORS origin url

--- crowpro/production.py
+++ crowpro/production.py
@@ -24,18 +24,21 @@ DATABASES = {
 CORS_ALLOWED_ORIGINS = [
     os.getenv("DEVELOPMENT_HOST"),
     "https://crowcrows-605q582yu-besoft33s-projects.vercel.app",
+    "https://crowcrows-web-besoft33s-projects.vercel.app",
     "https://www.santosh-bhattarai.com.np"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     os.getenv("DEVELOPMENT_HOST"),
     "https://crowcrows-605q582yu-besoft33s-projects.vercel.app",
+    "https://crowcrows-web-besoft33s-projects.vercel.app",
     "https://www.santosh-bhattarai.com.np"
 ]
 
 CORS_ORIGINS_WHITELIST = [
     os.getenv("DEVELOPMENT_HOST"),
     "https://crowcrows-605q582yu-besoft33s-projects.vercel.app",
+    "https://crowcrows-web-besoft33s-projects.vercel.app",
     "https://www.santosh-bhattarai.com.np"
 ]
 
