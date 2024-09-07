1e513c86e30e1bd31a735ce1dfd7a3b03466f9fb
BESoft33 <superstargajanikant@gmail.com>
2024-09-07 16:29:21 +0545

    modified cors urls

--- crowpro/production.py
+++ crowpro/production.py
@@ -8,7 +8,7 @@ SECRET_KEY = "".join(random.choices(string.ascii_letters +
                                     string.digits,
                                     k=20))
 
-ALLOWED_HOSTS = ['.vercel.app', 'now.sh', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh']
 
 DATABASES = {
     'default': {
@@ -23,21 +23,18 @@ DATABASES = {
 
 CORS_ALLOWED_ORIGINS = [
     os.getenv("DEVELOPMENT_HOST"),
-    "https://crowcrows-605q582yu-besoft33s-projects.vercel.app",
     "https://crowcrows-web-besoft33s-projects.vercel.app",
     "https://www.santosh-bhattarai.com.np"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     os.getenv("DEVELOPMENT_HOST"),
-    "https://crowcrows-605q582yu-besoft33s-projects.vercel.app",
     "https://crowcrows-web-besoft33s-projects.vercel.app",
     "https://www.santosh-bhattarai.com.np"
 ]
 
 CORS_ORIGINS_WHITELIST = [
     os.getenv("DEVELOPMENT_HOST"),
-    "https://crowcrows-605q582yu-besoft33s-projects.vercel.app",
     "https://crowcrows-web-besoft33s-projects.vercel.app",
     "https://www.santosh-bhattarai.com.np"
 ]
