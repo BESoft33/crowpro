ec11c038317cbdd35e46a27448267c5b02591271
BESoft33 <superstargajanikant@gmail.com>
2024-09-07 17:03:04 +0545

    modified staticfiles dirs

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,11 @@
-pip install -r requirements.txt
+echo "Installing dependencies"
+python3.9 -m pip install -r requirements.txt
+
+echo "Serving staticfiles"
 python3.9 manage.py collectstatic --noinput
+
+echo "Applying migrations"
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Build complete"
--- crowpro/settings.py
+++ crowpro/settings.py
@@ -146,7 +146,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles' / 'static'
 
 HIGHLIGHT_URL = 'highlight/'
 
