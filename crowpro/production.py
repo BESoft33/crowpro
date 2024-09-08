07c2013ff2ed6f2da65659167315f18500c246f4
BESoft33 <superstargajanikant@gmail.com>
2024-09-08 09:35:05 +0545

    database config

--- crowpro/production.py
+++ crowpro/production.py
@@ -13,12 +13,12 @@ ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '127.0.0.1']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("POSTGRES_DATABASE"),
         'USER': os.getenv("POSTGRES_USER"),
         'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
         'HOST': os.getenv("POSTGRES_HOST"),
-        # 'PORT': os.getenv("POSTGRES_PORT"),
+        'PORT': os.getenv("POSTGRES_PORT", 5432),
     }
 }
 
