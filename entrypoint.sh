4c4f15b972b7ac17a8adf219a92a9e68acad487d
BESoft33 <superstargajanikant@gmail.com>
2024-09-22 08:50:14 +0545

    relocated requirements for development setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@
 python -m pip install --upgrade pip
 python -m pip install --no-cache-dir -r requirements.txt
 
+# Requirements for development (no need if postgres is used)
+python -m pip install --no-cache-dir -r requirements.txt.dev
+
 # Run Django database migrations
 python manage.py makemigrations
 python manage.py migrate
