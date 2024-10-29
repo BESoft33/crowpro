961780948a6522a550c1a5dc8cdfed14b8d43d73
besoft33 <superstargajanikant@gmail.com>
2024-10-29 18:11:41 +0545

    skipped staticfiles collection in production

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,6 @@
 echo "Installing dependencies"
 python3.9 -m pip install -r requirements.txt
 
-python3.9 manage.py collectstatic --noinput
-
 echo "Applying migrations"
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
