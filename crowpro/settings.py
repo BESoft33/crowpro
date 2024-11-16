fd7ffa46450441f95c8aedf1e06435f89852dc16
besoft33 <superstargajanikant@gmail.com>
2024-11-16 20:20:16 +0545

    early refresh token expiry fixed

--- crowpro/settings.py
+++ crowpro/settings.py
@@ -160,7 +160,7 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": False,
-    "BLACKLIST_AFTER_ROTATION": False,
+    "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": True,
 }
 
